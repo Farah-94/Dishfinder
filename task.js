b884202f7ea69802c574c3f0c428ffc9f12aa2fb
Farah-94 <farahshigri94@gmail.com>
2025-01-26 19:17:31 +0000

    css added, two more pages created for order or reserve a table

--- task.js
+++ task.js
@@ -7,22 +7,36 @@ function smallScreen() { // this function is used to show the navigation bar in 
       x.className = "navigation"; // if the class name is not equal to navigation then it will set the class name to navigation
     }
   }
-  
-  $(document).ready(function () {
+
+  document.addEventListener('DOMContentLoaded', (event) => {
+    const imageSlider = document.querySelector('#image-slider .slides');
+    const reviewSlider = document.querySelector('#review-slider .slides');
+    const imageSlides = document.querySelectorAll('#image-slider .slide');
+    const reviewSlides = document.querySelectorAll('#review-slider .slide');
     let currentIndex = 0;
-    const slides = $('.slides');
-    const slideCount = $('.slide').length;
-  
-    function showNextSlide() {
-      currentIndex = (currentIndex + 1) % slideCount;
-      slides.css('transform', 'translateX(' + (-currentIndex * 100) + '%)');
+
+    function startSliders() {
+        setInterval(() => {
+            // Move to the next slide
+            currentIndex = (currentIndex + 1) % imageSlides.length;
+
+            // Apply the transform to both sliders
+            imageSlider.style.transform = `translateX(-${currentIndex * 100}%)`;
+            reviewSlider.style.transform = `translateX(-${currentIndex * 100}%)`;
+
+            // If we're at the end of the slides, add a delay
+            if (currentIndex === imageSlides.length - 1) {
+                setTimeout(() => {
+                    currentIndex = -1; // Reset index to start from 0 on the next interval
+                }, 4000); // Delay time in milliseconds
+            }
+        }, 5000); // Change slide every 4 seconds
     }
-  
-    setInterval(showNextSlide, 4000); // Change image every 4 seconds
-  });
+
+    startSliders();
+});
 
 
-//
   async function searchRestaurants() {
     const input = document.getElementById('searchInput').value.toLowerCase(); // Get the input value and convert it to lowercase
     const resultsDropdown = document.getElementById('resultsDropdown'); // Get the dropdown element inside select tag
@@ -54,5 +68,21 @@ function smallScreen() { // this function is used to show the navigation bar in 
   
   function optionFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+  function store() {
+    var selectedRestaurant = document.getElementById("resultsDropdown").value;
+    if (selectedRestaurant) {
+      localStorage.setItem("selectedRestaurant", selectedRestaurant);
+    }
+  }
+  
+
+
+  function displayRestaurantName() {
+    var restaurantName = localStorage.getItem("selectedRestaurant");
+    if (restaurantName) {
+      document.getElementById("restaurantName").innerText += restaurantName;
+    }
   }
   
