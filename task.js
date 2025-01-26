bf3f21ed95a1141a8be3e7972a4dc65ac3e9e16f
Farah-94 <farahshigri94@gmail.com>
2025-01-26 00:08:39 +0000

    css added

--- task.js
+++ task.js
@@ -1,10 +1,10 @@
 
-function clickFunction() {
+function smallScreen() { // this function is used to show the navigation bar in the small screen when the user click on the icon
     var x = document.getElementById("navbar");
-    if (x.className === "navigation") {
-      x.className += " responsive";
+    if (x.className === "navigation") { // check if the class name is equal to navigation
+      x.className += " responsive"; // add the class name navigation responsive
     } else {
-      x.className = "navigation";
+      x.className = "navigation"; // if the class name is not equal to navigation then it will set the class name to navigation
     }
   }
   $(document).ready(function () {
@@ -19,4 +19,36 @@ function clickFunction() {
   
     setInterval(showNextSlide, 4000); // Change image every 4 seconds
   });
+
+
+//
+  async function searchRestaurants() {
+    const input = document.getElementById('searchInput').value.toLowerCase(); // Get the input value and convert it to lowercase
+    const resultsDropdown = document.getElementById('resultsDropdown'); // Get the dropdown element inside select tag
+    // resultsDropdown.innerHTML = '<option value="">select </option>'; // you can rewrite in option tag
+    try { // it is used to handle the error
+        const response = await fetch(`http://localhost:3000/search?query=${input}`); // await is used to pause the execution until fetch completes 
+        const data = await response.json(); // await is used to pause the execution until data convert in json format
+        if (data.results.length > 0) { // check if the results has somthing and not empty
+            data.results.forEach(restaurant => { // loop through the results. forEach is a method that calls a function for each element in the array
+                const option = document.createElement('option'); // create an option element for each restaurant
+                option.value = restaurant.name; // set the value of the option to the restaurant name
+                option.textContent = `${restaurant.name} - Rating: ${restaurant.rating} - vicinity: ${restaurant.vicinity}`; //The text content includes the restaurant name, rating, and vicinity.
+                resultsDropdown.appendChild(option); //it is adding option element in the resultsDropdown and option element has value and textContent.
+            });
+        } else { // if the results is empty then it will show the message in the dropdown
+            const option = document.createElement('option');
+            option.value = ''; // it will set the value of the option to empty
+            option.textContent = 'No results found'; //it will show the message in the dropdown
+            resultsDropdown.appendChild(option); 
+        }
+    } catch (error) { // it will handle the error
+        console.error('Error:', error); // it will show the error in the console
+        const option = document.createElement('option'); // it will create the option element
+        option.value = '';      // it will set the value of the option to empty
+        option.textContent = 'An error occurred while fetching data.'; // it will show the message in the dropdown
+        resultsDropdown.appendChild(option); // it will add the option element in the resultsDropdown
+    }
+  }
+  
   
