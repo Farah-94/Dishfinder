067f2baf028208feabfc01942eae31a66f44d97d
Farah-94 <farahshigri94@gmail.com>
2025-04-08 23:35:37 +0100

    order

--- public/task.js
+++ public/task.js
@@ -59,14 +59,6 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-  const searchBtn = document.getElementById("searchBtn");
-  if (searchBtn) {
-    searchBtn.addEventListener('click', searchRestaurants);
-  } else {
-    console.error("Element with ID 'searchBtn' not found.");
-  }
-});
 
 async function searchRestaurants() {
   const input = document.getElementById('searchInput').value.toLowerCase();
